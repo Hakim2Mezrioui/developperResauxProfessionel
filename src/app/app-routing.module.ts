1e1f7c1f808d0434336c83bf062a0814bcc86c93
mezrioui hakim <mezrioui.hakim@gmail.com>
2024-01-16 10:27:31 +0100

    exposer du resaux professional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlanComponent } from './plan/plan.component';
+import { ImportanceComponent } from './importance/importance.component';
+import { ObjectifsComponent } from './objectifs/objectifs.component';
+import { OutilsComponent } from './outils/outils.component';
+import { ConseilsComponent } from './conseils/conseils.component';
+import { ConclusionComponent } from './conclusion/conclusion.component';
+import { FinalComponent } from './final/final.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: FinalComponent},
+  {path: "introduction", component: PlanComponent},
+  {path: "importance", component: ImportanceComponent},
+  {path: "objectifs", component: ObjectifsComponent},
+  {path: "outils", component: OutilsComponent},
+  {path: "conseils", component: ConseilsComponent},
+  {path: "conclusion", component: ConclusionComponent},
+  {path: "final", component: FinalComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanComponent } from './plan/plan.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { ImportanceComponent } from './importance/importance.component';
+import { ObjectifsComponent } from './objectifs/objectifs.component';
+import { OutilsComponent } from './outils/outils.component';
+import { ConseilsComponent } from './conseils/conseils.component';
+import { ConclusionComponent } from './conclusion/conclusion.component';
+import { FinalComponent } from './final/final.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanComponent,
+    HeaderComponent,
+    ImportanceComponent,
+    ObjectifsComponent,
+    OutilsComponent,
+    ConseilsComponent,
+    ConclusionComponent,
+    FinalComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
